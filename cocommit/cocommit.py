1fb9af0d5f6910718c7af49bc07fa45df5a3f3f8
Andrew Romanenco <andrew@romanenco.com>
2025-03-12 23:55:21 -0400

    Template for llm response processing
    This is temp code before wiring an actual call to an LLM. This is
    a UI test.

--- cocommit/cocommit.py
+++ cocommit/cocommit.py
@@ -1,5 +1,7 @@
+from cocommit.bedrock import call_bedrock_claude_3_7
 from cocommit.git_utils import get_last_commit_message, is_git_repo
 from cocommit.prompt_utils import get_llm_prompt
+from cocommit.response_utils import get_list_of_recommendations, get_updated_commit_message
 
 def main():
     path = "."
@@ -8,7 +10,24 @@ def main():
         return
     last_commit_message = get_last_commit_message('.')
     llm_prompt = get_llm_prompt(last_commit_message)
-    print(llm_prompt)
+    result = "<NEW-COMMIT-MESSAGE>the new message\n\nbody</NEW-COMMIT-MESSAGE> and <FIXES>first\nsecond</FIXES>"
+    new_commit_message = get_updated_commit_message(result)
+    recommendations = get_list_of_recommendations(result)
+    print("Full promt (for debug)")
+    print(result)
+    print("EndOfResult")
+    print("\n"*5)
+    if new_commit_message:
+        print("\t***Proposed commit message***\n\n")
+        print(new_commit_message)
+        print("\n\n\t***End of commit message***")
+    else:
+        print("\tNo commit message is available\n\n")
+    if len(recommendations) > 0:
+        print("Recommended fixes:")
+        for r in recommendations:
+            print(f" - {r}")
+    print("All done!")
 
 if __name__ == "__main__":
     main()
